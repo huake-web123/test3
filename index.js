1d8ef838e30e99a4bea9d1b96f0325bfa9e6ba93
smilehanxiao <413169485@qq.com>
2018-05-08 21:28:01 +0800

    写了vue的ajax

--- index.js
+++ index.js
@@ -6,11 +6,22 @@
  * 写出作者，如果别人有疑问，别人可以找到你咨询
  */
 
-new Vue({
+var myVue = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!',
     // musicClassArr: ['摇滚','古典','爵士','民谣/乡村','流行','电子','原声配乐','轻音乐','说唱','雷鬼'],
-    musicClassArr: [{name:'摇滚'},{name:'古典'},{name:'爵士'}]
+    musicClassArr: [{name:'摇滚'},{name:'古典'},{name:'爵士'}],
+    bannerArr:[]
   }
 })
+
+function getMusicBanners(){
+    return axios.get('http://restaurant.yijiahotel.shop/test/musicbanners');
+}
+axios.all([getMusicBanners()])
+    .then(axios.spread(function(banner){
+    	console.log(banner);
+        //请求现在都完成
+        myVue.bannerArr = banner.data.data
+    }));
