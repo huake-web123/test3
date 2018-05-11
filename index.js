130db3e167ff0c98a3a5a60fa69405e03e5c3c35
smilehanxiao <413169485@qq.com>
2018-05-11 17:37:54 +0800

    添加了vue获取兆赫部分的数据

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ var myVue = new Vue({
     message: 'Hello Vue.js!',
     // musicClassArr: ['摇滚','古典','爵士','民谣/乡村','流行','电子','原声配乐','轻音乐','说唱','雷鬼'],
     musicClassArr: [{name:'摇滚'},{name:'古典'},{name:'爵士'}],
-    bannerArr:[]
+    bannerArr:[],
+    hotMhzArr:[],
+    fastMhzArr:[]
   }
 })
-
+/*获取banner的数据*/
 function getMusicBanners(){
     return axios.get('http://restaurant.yijiahotel.shop/test/musicbanners');
 }
@@ -25,3 +27,23 @@ axios.all([getMusicBanners()])
         //请求现在都完成
         myVue.bannerArr = banner.data.data
     }));
+
+/*获取热门兆赫数据*/
+function getMusicHotMhz(){
+	return axios.get('http://restaurant.yijiahotel.shop/test/musicHotMhz');
+}
+axios.all([getMusicHotMhz()])
+	.then(axios.spread(function(hotMhz){
+		console.log(hotMhz);
+		myVue.hotMhzArr = hotMhz.data.data
+	
+	}));
+
+/*获取上升最快兆赫数据*/
+function getMusicFastMhz(){
+	return axios.get('http://restaurant.yijiahotel.shop/test/musicFastMhz');
+}
+axios.all([getMusicFastMhz()])
+	.then(axios.spread(function(fastMhz){
+		myVue.fastMhzArr = fastMhz.data.data
+	}));
