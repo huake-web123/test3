230de3fea79789432631866179ae618df50cfe8c
smilehanxiao <413169485@qq.com>
2018-05-12 00:43:05 +0800

    切换index的方法事件

--- index.js
+++ index.js
@@ -13,37 +13,42 @@ var myVue = new Vue({
     // musicClassArr: ['摇滚','古典','爵士','民谣/乡村','流行','电子','原声配乐','轻音乐','说唱','雷鬼'],
     musicClassArr: [{name:'摇滚'},{name:'古典'},{name:'爵士'}],
     bannerArr:[],
+    arr:[1,2,3,4,5],
+    currentIndex:0,
     hotMhzArr:[],
     fastMhzArr:[]
+  },
+  methods:{
+  	pageNumEnter:function(index) {
+  		this.currentIndex = index
+  	},
+  	pageNumOut:function() {
+  		
+  	}
   }
 })
 /*获取banner的数据*/
 function getMusicBanners(){
     return axios.get('http://restaurant.yijiahotel.shop/test/musicbanners');
 }
-axios.all([getMusicBanners()])
-    .then(axios.spread(function(banner){
-    	console.log(banner);
-        //请求现在都完成
-        myVue.bannerArr = banner.data.data
-    }));
+
 
 /*获取热门兆赫数据*/
 function getMusicHotMhz(){
 	return axios.get('http://restaurant.yijiahotel.shop/test/musicHotMhz');
 }
-axios.all([getMusicHotMhz()])
-	.then(axios.spread(function(hotMhz){
-		console.log(hotMhz);
-		myVue.hotMhzArr = hotMhz.data.data
-	
-	}));
 
 /*获取上升最快兆赫数据*/
 function getMusicFastMhz(){
 	return axios.get('http://restaurant.yijiahotel.shop/test/musicFastMhz');
 }
-axios.all([getMusicFastMhz()])
-	.then(axios.spread(function(fastMhz){
-		myVue.fastMhzArr = fastMhz.data.data
-	}));
+
+axios.all([getMusicBanners(),getMusicHotMhz(),getMusicFastMhz()])
+    .then(axios.spread(function(banner,hotMhz,fastMhz){
+    	console.log(banner);
+        //请求现在都完成
+        
+        myVue.fastMhzArr = fastMhz.data.data
+        myVue.bannerArr = banner.data.data
+        myVue.hotMhzArr = hotMhz.data.data
+    }));
